b819c1e4709b8b1fd1fcb25cd4ead69de1d12f86
draxrunner <jailtonsilva.v@gmail.com>
2026-07-03 20:52:09 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -6,3 +6,7 @@ const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+const supabaseUrl = "https://dputivbehhvbqeonyjdz.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwdXRpdmJlaGh2YnFlb255amR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMTA0ODMsImV4cCI6MjA5ODY4NjQ4M30.lNqaqLXp_jGp8aFjnWhMAbYlSKKXGUfh-l5U8CF0WqQ"
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
